32a904da5ca8c8e86676cf1a3faad261758c50ec
Nika Orlova <nika.orlova.1997@yandex.ru>
2020-01-19 17:26:24 +0300

    fix code style and add task2_2

--- task2/task2.ts
+++ task2/task2.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
 import { v1 as uuid } from 'uuid';
+import Joi from '@hapi/joi';
 
 type User = {
   id: string;
@@ -16,20 +17,48 @@ function getAutoSuggestUsers(
   limit: number
 ): Array<User> {
   return users
-    .filter(user => !user.isDeleted && user.login.includes(loginSubstring))
-    .sort((a, b) => a.login.localeCompare(b.login))
+    .filter(
+      (user: User) => !user.isDeleted && user.login.includes(loginSubstring)
+    )
+    .sort((a: User, b: User) => a.login.localeCompare(b.login))
     .slice(0, limit);
 }
 
-const app = express();
+const schema: Joi.Schema = Joi.object({
+  login: Joi.string().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9]{2,}$'))
+    .required(),
+  age: Joi.number()
+    .integer()
+    .min(4)
+    .max(130)
+    .required()
+});
+
+function validateUser() {
+  return (req: Request, res: Response, next: NextFunction): boolean | void => {
+    const { error } = schema.validate(req.body);
+
+    if (error && error.isJoi) {
+      res.status(400).json(error.message);
+      return false;
+    }
+    next();
+  };
+}
+
+const app: express.Application = express();
 app.listen(3000, () => {
   console.info('Server is started');
 });
 app.use(express.json());
 
 // get user by id
-app.get('/users/:id', (req, res) => {
-  const user = users.find((user: User) => user.id === req.params.id);
+app.get('/users/:id', (req: Request, res: Response) => {
+  const user: User | undefined = users.find(
+    (u: User) => u.id === req.params.id
+  );
   if (!user) {
     res.status(404).json({ mesage: 'User not found' });
   }
@@ -37,36 +66,36 @@ app.get('/users/:id', (req, res) => {
 });
 
 // create user
-app.post('/users', (req, res) => {
+app.post('/users', validateUser(), (req: Request, res: Response) => {
   const user: User = req.body;
   user.id = uuid();
   user.isDeleted = false;
   users.push(user);
 
-  res.location('/users/' + user.id);
+  res.location(`/users/${user.id}`);
   res.status(201).json(user);
 });
 
 // update user
-app.put('/users/:id', (req, res) => {
-  const id = req.params.id;
-  let user = users.find(user => user.id === id);
+app.put('/users/:id', validateUser(), (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const user: User | undefined = users.find((u: User) => u.id === id);
 
   if (!user) {
     res.status(404).send('User is not found');
   } else {
-    user.login = req.body.login || user.login;
-    user.password = req.body.password || user.password;
-    user.age = req.body.age || user.age;
+    user.login = req.body.login;
+    user.password = req.body.password;
+    user.age = req.body.age;
 
     res.json(user);
   }
 });
 
 // get user list by limit (number of users) and loginSubstring
-app.get('/users', (req, res) => {
+app.get('/users', (req: Request, res: Response) => {
   const { loginSubstring = '', limit = users.length } = req.query;
-  const list = getAutoSuggestUsers(loginSubstring, limit);
+  const list: User[] = getAutoSuggestUsers(loginSubstring, limit);
   if (!list || list.length === 0) {
     res.status(404).json({ mesage: 'There no users for this request' });
   } else {
@@ -75,9 +104,9 @@ app.get('/users', (req, res) => {
 });
 
 // remove user
-app.delete('/users/:id', (req, res) => {
-  const id = req.params.id;
-  let user = users.find(user => user.id === id);
+app.delete('/users/:id', (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const user: User | undefined = users.find((u: User) => u.id === id);
 
   if (!user || user.isDeleted) {
     res.status(404).send('User is not found');
