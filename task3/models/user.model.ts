08ae3a3d196be7a0f28e26e045bd8b324a0fe69f
Nika Orlova <nika.orlova.1997@yandex.ru>
2020-02-06 16:52:49 +0300

    add validation

--- task3/models/user.model.ts
+++ task3/models/user.model.ts
@@ -7,6 +7,8 @@ import {
   Default,
   DataType
 } from 'sequelize-typescript';
+import Joi from '@hapi/joi';
+import { NextFunction, Response, Request } from 'express';
 
 export interface IUser {
   id: string;
@@ -41,3 +43,29 @@ export class UserModel extends Model<UserModel> {
   @Column(DataType.BOOLEAN)
   isDeleted: boolean | undefined;
 }
+
+const schema: Joi.ObjectSchema<IUser> = Joi.object({
+  login: Joi.string().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9]{2,}$'))
+    .required(),
+  age: Joi.number()
+    .integer()
+    .min(4)
+    .max(130)
+    .required()
+});
+
+const validate = (schema: Joi.ObjectSchema<IUser>) => {
+  return (req: Request, res: Response, next: NextFunction): boolean | void => {
+    const { error } = schema.validate(req.body);
+
+    if (error && error.isJoi) {
+      res.status(400).json(error.message);
+      return false;
+    }
+    next();
+  };
+};
+
+export const validateUser = validate(schema);
